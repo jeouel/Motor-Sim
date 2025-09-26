91e33433fd22cb83b602155fc6c2f13cc554cde5
jeouel <jerome.ouellette@gmail.com>
2025-09-26 18:44:00 -0400

    fragment colors can now be adjusted, needs to be per mesh

--- src/shader.cpp
+++ src/shader.cpp
@@ -39,6 +39,10 @@ void Shader::setMat4(const std::string &name, const glm::mat4 &mat) {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
 }
 
+void Shader::setVec4(const std::string &name, const glm::vec4 &vec) {
+        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(vec));
+};
+
 std::string Shader::loadFile(const char* path) {
         std::ifstream file;
         std::stringstream stream;
--- src/shader.h
+++ src/shader.h
@@ -10,7 +10,8 @@ public:
         Shader(const char* vertexPath, const char* fragmentPath);
         void use();
         void setMat4(const std::string &name, const glm::mat4 &mat);
-    
+        void setVec4(const std::string &name, const glm::vec4 &vec);
+
 private:
         std::string loadFile(const char* path);
         void checkCompileErrors(unsigned int shader, std::string type);
